03623cc58aa0e614aa6e3cabcbdb7212abd8d6b7
Landon Tyler Bennett <landonepgy@gmail.com>
2015-04-15 13:27:49 -0700

    mod for html and js

--- cutest_kitten.js
+++ cutest_kitten.js
@@ -1,41 +1,76 @@
 (function(){
-var url = '/kittens/';
 
-var random_kitties = ["./kittens/kitten1.jpg", "./kittens/kitten2.jpg", "./kittens/kitten3.jpg", "./kittens/kitten4.jpg", "./kittens/kitten5.jpg", "./kittens/kitten6.jpg", "./kittens/kitten7.jpg", "./kittens/kitten8.jpg", "./kittens/kitten9.jpg", "./kittens/kitten10.jpg", "./kittens/kitten11.jpg", "./kittens/kitten12.jpg", "./kittens/kitten13.jpg", "./kittens/kitten14.jpg"];
+var randomKittens = [];
 
- function randomImg(imgAr, path) {
-   path = path || url;
+
+var generateKittenObjs = function(numKittens) {
+   for(var i=1; i <= numKittens; i++) {
+      var kitten = {
+         imgUrl: './kittens/kitten' + i + '.jpg',
+         votes: 0,
+         timesShown: 0
+      };
+      randomKittens.push(kitten);
+   }
+}
+
+ function getTwoKittens(imgAr) {
+
+   // Clears the div for new cat photos.
+   var kittenDiv = document.getElementById('kittens');
+   kittenDiv.innerHTML = "";
+
    var num = Math.floor(Math.random() * imgAr.length);
    var num2 = Math.floor(Math.random() * imgAr.length);
 
-   while(num === num2) {
+  while(num === num2) {
     num2 = Math.floor(Math.random() * imgAr.length);
    }
 
+   var kitten1 = imgAr[num];
+   var kitten2 = imgAr[num2];
+
    var img1 = document.createElement('img');
    img1.setAttribute('id', 'imgOne');
+   img1.src = kitten1.imgUrl;
+
    var img2 = document.createElement('img');
    img2.setAttribute('id', 'imgTwo');
+   img2.src = kitten2.imgUrl;
 
-   img1.src = imgAr[num];
-   img2.src = imgAr[num2];
 
-   document.getElementById('kitten1').appendChild(img1);
-   document.getElementById('kitten2').appendChild(img2);
+   kittenDiv.appendChild(img1);
+   kittenDiv.appendChild(img2);
 
-   var kitten1 = document.getElementById('imgOne');
-   var kitten2 = document.getElementById('imgTwo');
+   var kitten1image = document.getElementById('imgOne');
+   var kitten2image = document.getElementById('imgTwo');
 
-   kitten1.addEventListener('click', function() {
-      kitten1.style.border = "5px solid blue";
+   kitten1image.addEventListener('click', function() {
+      kitten1image.style.border = "5px solid blue";
+
+      kitten1.votes++;
+
+      kitten1.timesShown++;
+      kitten2.timesShown++;
+
+      setTimeout(getTwoKittens, 500, randomKittens);
+
    });
-   kitten2.addEventListener('click', function() {
-      kitten2.style.border = "5px solid red";
-   });
+   kitten2image.addEventListener('click', function() {
+      kitten2image.style.border = "5px solid red";
+
+      kitten2.votes++;
+
+      kitten2.timesShown++;
+      kitten1.timesShown++;
+
+      setTimeout(getTwoKittens, 500, randomKittens);
+
+    });
  };
 
+generateKittenObjs(14);
 
-
-randomImg(random_kitties, url);
+getTwoKittens(randomKittens);
 
 }());
