592e767c47dfdb1484178efa8fe1ed50a3beb5b4
Landon Tyler Bennett <landonepgy@gmail.com>
2015-04-15 16:06:32 -0700

    html/js

--- cutest_kitten.js
+++ cutest_kitten.js
@@ -2,6 +2,55 @@
 
 var randomKittens = [];
 
+function kittenChartVotes(kitten1Votes, kitten2Votes) {
+
+  var data = [
+    {
+        value: kitten1Votes,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: kitten2Votes,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    ];
+ var options = {
+ //Boolean - Whether we should show a stroke on each segment
+       segmentShowStroke : true,
+
+       //String - The colour of each segment stroke
+       segmentStrokeColor : "#fff",
+
+       //Number - The width of each segment stroke
+       segmentStrokeWidth : 2,
+
+       //Number - The percentage of the chart that we cut out of the middle
+       percentageInnerCutout : 50, // This is 0 for Pie charts
+
+       //Number - Amount of animation steps
+       animationSteps : 100,
+
+       //String - Animation easing effect
+       animationEasing : "easeOutBounce",
+
+       //Boolean - Whether we animate the rotation of the Doughnut
+       animateRotate : true,
+
+       //Boolean - Whether we animate scaling the Doughnut from the centre
+       animateScale : false,
+
+       //String - A legend template
+       legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
+
+};
+
+var ctx = document.getElementById("kittenChart").getContext("2d");
+var myDoughnutChart = new Chart(ctx).Doughnut(data,options);
+}
 
 var generateKittenObjs = function(numKittens) {
    for(var i=1; i <= numKittens; i++) {
@@ -13,6 +62,8 @@ var generateKittenObjs = function(numKittens) {
       randomKittens.push(kitten);
    }
 }
+
+
 
  function getTwoKittens(imgAr) {
 
@@ -55,6 +106,9 @@ var generateKittenObjs = function(numKittens) {
 
       setTimeout(getTwoKittens, 500, randomKittens);
 
+      kittenChartVotes(kitten1.votes, kitten2.votes);
+
+
    });
    kitten2image.addEventListener('click', function() {
       kitten2image.style.border = "5px solid red";
@@ -66,7 +120,9 @@ var generateKittenObjs = function(numKittens) {
 
       setTimeout(getTwoKittens, 500, randomKittens);
 
-    });
+      kittenChartVotes(kitten1.votes, kitten2.votes);
+
+   });
  };
 
 generateKittenObjs(14);
